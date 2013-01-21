713d3af14db17c51d757fc434544894b9d0f1fb5
Lee Reilly <lee@leereilly.net>
2013-01-21 15:55:24 -0800

    A little refactoring :sparkles:

--- lib/mx_record.rb
+++ lib/mx_record.rb
@@ -1,17 +1,33 @@
 require 'resolv'
 
 module MXRecord
-  def self.get_server(domain)
-    mx = nil
+
+  # Obtain the mail exchange record for a particular domain.
+  #
+  # Returns an array of Resolv::DNS::Resource::IN::MX records e.g. for Google:
+  #
+  # [ #<Resolv::DNS::Resource::IN::MX:0x007faaac870da8 @preference=50, @exchange=#<Resolv::DNS::Name: alt4.aspmx.l.google.com.>, @ttl=347>,
+  #   #<Resolv::DNS::Resource::IN::MX:0x007faaac8769b0 @preference=20, @exchange=#<Resolv::DNS::Name: alt1.aspmx.l.google.com.>, @ttl=347>,
+  #   #<Resolv::DNS::Resource::IN::MX:0x007faaac874db8 @preference=40, @exchange=#<Resolv::DNS::Name: alt3.aspmx.l.google.com.>, @ttl=347>,
+  #   #<Resolv::DNS::Resource::IN::MX:0x007faaac87abf0 @preference=30, @exchange=#<Resolv::DNS::Name: alt2.aspmx.l.google.com.>, @ttl=347>,
+  #   #<Resolv::DNS::Resource::IN::MX:0x007faaac87f830 @preference=10, @exchange=#<Resolv::DNS::Name: aspmx.l.google.com.>, @ttl=347> ]
+  def self.mail_servers(domain)
     Resolv::DNS.open do |dns|
-      mail_servers = dns.getresources(domain, Resolv::DNS::Resource::IN::MX)
-      return nil unless mail_servers and not mail_servers.empty?
-      highest_priority = mail_servers.first
-      mail_servers.each do |server|
-        highest_priority = server if server.preference < highest_priority.preference
-      end
-      mx = highest_priority.exchange.to_s
+      dns.getresources(domain, Resolv::DNS::Resource::IN::MX)
     end
-    return mx
+  end
+
+  # Obtain the preferred mail server based on the MX record's preference.
+  #
+  # Returns a String containing the mail server name e.g. aspmx.l.google.com.
+  def self.primary_server(domain)
+    servers = mail_servers(domain)
+    primary_server = servers.first
+
+    servers.each do |server|
+      primary_server = server if server.preference < primary_server.preference
+    end
+
+    primary_server.exchange.to_s
   end
 end
--- test/test_mx_record.rb
+++ test/test_mx_record.rb
@@ -1,8 +1,14 @@
 require 'helper'
 
 class TestMxRecord < Test::Unit::TestCase
-  should "probably rename this file and start testing for real" do
-    mx_server = MXRecord::get_server('google.com')
+  should "obtain the highest priority mail server" do
+    mx_server = MXRecord::primary_server('google.com')
     assert_equal 'aspmx.l.google.com', mx_server
   end
+
+  should "obtain all of the mail servers" do
+    mx_servers = MXRecord::mail_servers('google.com')
+    assert_equal 5, mx_servers.size
+  end
+
 end
